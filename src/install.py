6c69931d3e3b800db4f3f4d752cd7d7c9a22773d
readysloth <readysloth@protonmail.com>
2024-02-16 22:51:06 +0300

    feat(install.py): Added function for logging setup and chroot pretending

--- src/install.py
+++ src/install.py
@@ -1,16 +1,31 @@
+import os
 import json
+import logging
 import subprocess as sp
 
-import cmd
 import disk
 import stage3
+import pkg
+
+from cursed_handler import CursesHandler
+
+from cmd import ShellCmd
+
+CHROOT_SCRIPT_PRETEND = [
+    ShellCmd('cat > chroot_script.sh << EOF\n%placeholder%\nEOF',
+             name='chroot script'),
+    ShellCmd('cp chroot_script.sh /mnt/gentoo',
+             name='chroot script copy'),
+    ShellCmd('chroot /bin/bash /chroot_script.sh',
+             name='chroot install')
+]
 
 
 def get_part(disk_node: str, number: int, pretend: bool = False) -> str:
     number -= 1
     if pretend:
         return f'{disk_node}{number}'
-    blkid = cmd.ShellCmd(f'lsblk --json --output NAME --paths {disk_node}')
+    blkid = ShellCmd(f'lsblk --json --output NAME --paths {disk_node}')
     proc = blkid(pretend=pretend, stdout=sp.PIPE)
     description = json.load(proc.stdout)
     return description['blockdevices'][0]['children'][number]['name']
@@ -29,12 +44,25 @@ def disk_stage(disk_node: str, pretend: bool = False):
     return executed_cmds
 
 
-def stage3_install(pretend: bool = False):
-    return stage3.stage3(pretend=pretend)
-
-
 def install(disk_node: str, pretend: bool = False):
     executed_cmds = []
     executed_cmds += disk_stage(disk_node, pretend=pretend)
     executed_cmds += stage3.stage3(pretend=pretend)
+
+    if not pretend:
+        os.chroot('/mnt/gentoo')
+        os.chdir('/')
+
+    cmds_in_chroot = []
+    for cmd in CHROOT_SCRIPT_PRETEND:
+        cmds_in_chroot.append(cmd(pretend=True))
+
+    executed_cmds += (cmds_in_chroot, CHROOT_SCRIPT_PRETEND)
     return executed_cmds
+
+
+def setup_logging():
+    logger = logging.getLogger()
+    curses_handler = CursesHandler()
+    logger.setLevel(logging.INFO)
+    logger.addHandler(curses_handler)
