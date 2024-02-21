8846e609521b1d4a55a81be6e455608829f202e5
readysloth <readysloth@protonmail.com>
2024-02-21 22:43:48 +0300

    add(download_scripts.sh)

---
+++ download_scripts.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+SCRIPTS="
+create_configs.sh
+cmd.py
+conf_files.py
+cursed_handler.py
+disk.py
+install.py
+pkg.py
+run.py
+stage3.py"
+
+FOLDER=installation_scripts
+
+mkdir "$FOLDER"
+cd "$FOLDER" || exit
+  for script in $SCRIPTS
+  do
+    wget "https://raw.githubusercontent.com/readysloth/better-gentoo-autoinstall/master/src/$script"
+  done
+cd - || exit
+
+echo "++++++++++++++++++++++++"
+echo "+ Download is complete +"
+echo "++++++++++++++++++++++++"
+echo
+echo "Go to folder '$FOLDER' and run 'run.py'"
