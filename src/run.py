967f5c6f7dfa0d6f35fc3327cd0fc119479cdb01
readysloth <readysloth@protonmail.com>
2024-04-11 20:07:35 +0300

    fix(src/run.py): Returned accidentally deleted `setup_logging`

--- src/run.py
+++ src/run.py
@@ -41,6 +41,8 @@ def main(args):
         for line in install.pretend_script(args.disk):
             print(line)
         return
+
+    install.setup_logging()
     logger = logging.getLogger()
     try:
         install.install(args.disk)
