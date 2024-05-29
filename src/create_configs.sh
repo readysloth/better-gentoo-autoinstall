f6bea01fb11787e715f979d7018bc5e45f8f0e15
readysloth <readysloth@protonmail.com>
2024-05-29 19:24:24 +0300

    fix(src/create_configs.sh): Forge referer

--- src/create_configs.sh
+++ src/create_configs.sh
@@ -381,7 +381,7 @@ cat << EOF >> /etc/privoxy/user.action
 +hide-from-header{block} \
 +hide-user-agent{Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36} \
 +hide-if-modified-since{-60} \
-+hide-referrer{https://www.google.com} \
++hide-referrer{forge} \
 +overwrite-last-modified{randomize} \
 +set-image-blocker{blank} \
 }
